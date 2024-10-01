5553bf0f0296a8a91bcaf26a48944c2ae5173973
Manuel <0139@htl.rennweg.at>
2024-10-01 11:47:20 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import '$lib/stylesheet.scss';
+    /*
+	import IconContainer from '$lib/Icons/Navbar/iconContainer.svelte';
+    */
+	import { afterNavigate } from '$app/navigation';
+
+	let url = '/';
+	afterNavigate((navigation) => {
+		url = navigation.to.url.pathname.toString();
+	});
+</script>
+
+<div class="pageDiv">
+	<div id="navbar">
+		<div class="navbarTextDiv">
+			<a class={url === '/' ? 'aSelected' : 'aDefault'} href="/">Grundkonfig</a>
+			<a class={url.startsWith('/Switchconfig') ? 'aSelected' : 'aDefault'} href="/Switchconfig">Switchconfig</a>
+			<a class={url.startsWith('/Routerconfig') ? 'aSelected' : 'aDefault'} href="/Routerconfig">Routerconfig</a>
+		</div>
+	</div>
+	<div class="statisticDiv">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "$lib/stylesheet.scss"
+
+</script>
+
+
+
