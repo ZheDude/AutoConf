ef37ab07878eb625f85ad5e03b2b071e1befe9ea
mehmet <0129@htl.rennweg.at>
2024-10-01 12:41:08 +0200

    basic implementation of the connections.py classes

--- Backend/connections.py
+++ Backend/connections.py
@@ -1,13 +1,55 @@
-import telnetlib3 as tlnt
+# this class is used to connect to a singular network device via SSH
+# every object of this class represents a connection to a singular network device
+# the class is used to send commands to the device and receive the output
+# the class is also used to close the connection to the device
 
-class Connection:
-    tn = None
+import socket
+from quopri import decodestring
+
+import paramiko
+import asyncio_telnet as Telnet
+
+class SSHConnection:
+    def __init__(self, ip, username, password):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            self.client.connect(self.ip, username=self.username, password=self.password)
+        except paramiko.ssh_exception.AuthenticationException:
+            raise Exception("Authentication failed")
+        except socket.gaierror:
+            raise Exception("Invalid IP address")
+        except paramiko.ssh_exception.NoValidConnectionsError:
+            raise Exception("Connection failed")
+        except paramiko.ssh_exception.BadHostKeyException:
+            raise Exception("Connection failed")
+        except paramiko.ssh_exception.SSHException:
+            raise Exception("Connection failed")
+
+    def send_command(self, command):
+        stdin, stdout, stderr = self.client.exec_command(command)
+        return stdout.read().decode("utf-8")
+
+    def close(self):
+        self.__del__()
     
-    def __init__(self, host, port = 23):
-        self.host = host
-        self.port = port
-        self.tn = tlnt.open_connection(host=host, port=port)
+    def __del__(self):
+        self.client.close()
         
-    def send(self, data): 
-        self.tn.send(data)
     
+class TelnetConnection:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.tn = Telnet.Telnet()
+    
+    async def send_command(self, command):
+        await self.tn.open(self.ip, self.port)
+        await self.tn.write(decodestring(command))
+        response = await self.tn.read_until_eof()
+        await self.tn.close()
+        return response
+            
--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,14 @@
-
-
+import asyncio
 
 def main():
-    print('HELLO FORTNITE')
-
-
+    from connections import SSHConnection
+    from connections import TelnetConnection
+    dev1 = TelnetConnection("172.16.244.128", 5000)
+    result = asyncio.run(dev1.send_command("do show version\n    "))
+    print(result)
+    
 if __name__ == '__main__':
     main()
+    
 
 
