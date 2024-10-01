f9d805cd88477f14b59d1de3f252d46965222446
sai <0148@htl.rennweg.at>
2024-10-01 11:07:28 +0200

    added some stuff to example json file

--- Backend/api.py
+++ Backend/api.py
@@ -1,8 +1,20 @@
-import fastapi
+from fastapi import FastAPI, Request
+import uvicorn
 
-def main():
-    app = fastapi.FastAPI()
+app = FastAPI()
 
 
-if __name__ == '__main__':
-    main()
+def process_data(data: dict):
+    print("Processing data:", data)
+    return {"processed_data": data}
+
+
+@app.post("/process/")
+async def receive_json(request: Request):
+    json_data = await request.json()
+    result = process_data(json_data)
+    return result
+
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
