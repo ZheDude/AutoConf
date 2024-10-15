46a63734229b7b8946ed2c88d07fda1bd0a151c0
mehmet <0129@htl.rennweg.at>
2024-10-15 12:42:31 +0200

    removed telnet

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,7 @@ import asyncio
 
 def main():
     from connections import SSHConnection
-    from connections import TelnetConnection
+    # from connections import TelnetConnection
     dev1 = SSHConnection("192.168.10.254", "cisco", "cisco")
     result = dev1.send_command("do show version\n    ")
     print(result)
