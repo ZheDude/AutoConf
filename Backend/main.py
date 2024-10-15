75c1e2a70c0e90f93bcdf73e1751ed6c5b0c7563
mehmet <0129@htl.rennweg.at>
2024-10-15 12:38:11 +0200

    small changes

--- Backend/main.py
+++ Backend/main.py
@@ -4,8 +4,8 @@ import asyncio
 def main():
     from connections import SSHConnection
     from connections import TelnetConnection
-    dev1 = TelnetConnection("172.16.244.128", 5000)
-    result = asyncio.run(dev1.send_command("do show version\n    "))
+    dev1 = SSHConnection("192.168.10.254", "cisco", "cisco")
+    result = dev1.send_command("do show version\n    ")
     print(result)
 
 
