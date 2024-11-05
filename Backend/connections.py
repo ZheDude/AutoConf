c86ab4100c35f971a445781fa795de85acb9fc43
mehmet <mehmetbalikci460@gmail.com>
2024-11-05 12:09:02 +0100

    fixed ssh session issue

--- Backend/connections.py
+++ Backend/connections.py
@@ -14,6 +14,7 @@ class SSHConnection:
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
             self.client.connect(self.ip, username=self.username, password=self.password)
+            self.channel = self.client.invoke_shell()
         except paramiko.ssh_exception.AuthenticationException:
             raise Exception("Authentication failed")
         except socket.gaierror:
@@ -26,8 +27,22 @@ class SSHConnection:
             raise Exception("Connection failed SSHException")
 
     def send_command(self, command):
-        stdin, stdout, stderr = self.client.exec_command(command)
-        return stdout.read().decode("utf-8")
+        if not self.channel:  # Ensure the channel is open
+            raise Exception("SSH session not active")
+
+        # Send command and read output
+        self.channel.send(command + '\n')
+        output = ""
+
+        while not self.channel.recv_ready():
+            pass  # Wait until data is ready to be read
+
+        while self.channel.recv_ready():
+            output += self.channel.recv(1024).decode("utf-8")
+
+        return output
+        # stdin, stdout, stderr = self.client.exec_command(command)
+        # return stdout.read().decode("utf-8")
 
     def close(self):
         self.__del__()
--- Backend/main.py
+++ Backend/main.py
@@ -4,7 +4,7 @@ import asyncio
 def main():
     from connections import SSHConnection
     # from connections import TelnetConnection
-    dev1 = SSHConnection("172.16.2.1", "cisco", "cisco")
+    dev1 = SSHConnection("172.16.2.127", "cisco", "cisco")
     result = dev1.send_command("do show version\n    ")
     print(result)
 
