fa5d83bdc4ec146d8d479b2d43905f212e636ba5
Sai <105285455+SaiMurali2006@users.noreply.github.com>
2024-11-05 13:24:21 +0100

    created some files

--- Backend/key_chain.py
+++ Backend/key_chain.py
@@ -1,4 +1,9 @@
+import json
 
+class KEYCHAINgenerator:
+    def __init__(self, key_chain_data, template_file):
+        ...
 
 if __name__ == "__main__":
-    ...
+    keychain_gen = KEYCHAINgenerator(key_chain_data, "Configurations/ospf_template.txt")
+
