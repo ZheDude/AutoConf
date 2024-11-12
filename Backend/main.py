849ba0087472d3473fd010e058186a1ec3a2daa8
mehmet <mehmetbalikci460@gmail.com>
2024-11-12 10:39:53 +0100

    small changes to SSH Connections

--- Backend/main.py
+++ Backend/main.py
@@ -5,11 +5,11 @@ def main():
     from connections import SSHConnection
     # from connections import TelnetConnection
     dev1 = SSHConnection("172.16.2.127", "cisco", "cisco")
-    result = dev1.send_command("configure terminal\n    ")
+    result = dev1.send_command_imprvd("configure terminal\n    ")
     print(result)
-    result = dev1.send_command("do-exec show version\n    ")
+    result = dev1.send_command_imprvd("do-exec show version\n    ")
     print(result)
-    result = dev1.send_command("do-exec show ip interface brief\n    ")
+    result = dev1.send_command_imprvd("do-exec show ip interface brief\n    ")
     print(result)
 
 if __name__ == '__main__':
