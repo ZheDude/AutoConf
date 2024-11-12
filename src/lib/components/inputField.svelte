893dc6ad07c0a3a3888c8654059d339ad6d7fe2c
BenBrendel <0130@htl.rennweg.at>
2024-11-12 11:38:07 +0100

    designed inputfielddiv

--- src/lib/components/inputField.svelte
+++ src/lib/components/inputField.svelte
@@ -1,18 +1,17 @@
-
 <script>
-    export let type = "text";
-    export let fieldName;
-    export let placeholder;
-    export let value = ''; 
-    export let id = '';
+	export let type = 'text';
+	export let fieldName;
+	export let placeholder;
+	export let value = '';
+	export let id = '';
 </script>
 
 <div class="inputFieldDiv">
-<label for="{id}">{fieldName}</label>
+	<label for={id}>{fieldName}</label>
 
-{#if type=== "text"}
-<input  bind:value={value} placeholder="{placeholder}" id="{id}" type="text" >
-{:else}
-<input  bind:value={value} placeholder="{placeholder}" id="{id}" type="password" >
-{/if}
+	{#if type === 'text'}
+		<input bind:value {placeholder} {id} type="text" />
+	{:else}
+		<input bind:value {placeholder} {id} type="password" />
+	{/if}
 </div>
