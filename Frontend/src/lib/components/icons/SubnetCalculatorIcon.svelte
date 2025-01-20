ef583065b0af22803b2a592dea2ddce2aa63a379
administrator <0139@htl.rennweg.at>
2025-01-20 20:55:19 +0100

    improved Subnetcalulator Icon

--- Frontend/src/lib/components/icons/SubnetCalculatorIcon.svelte
+++ Frontend/src/lib/components/icons/SubnetCalculatorIcon.svelte
@@ -1,79 +1,40 @@
 <script>
-    export let url;
-   let color;
-   $: color =  url === '/SubnetCalculator' ? "#a9dfd8" : "#87888C";
+	export let url;
+	let color;
+	$: color = url === '/SubnetCalculator' ? '#a9dfd8' : '#87888C';
 </script>
 
 <div id="RouterIconDiv">
-   <a href="/SubnetCalculator">
-<svg width="33" height="33" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-   <path d="M1 13.3612V11.1535C1 10.5439 1.5088 10.0497 2.13643 10.0497H14.6371C15.2648 10.0497 15.7736 10.5439 15.7736 11.1535V13.3612C15.7736 13.9708 15.2648 14.465 14.6371 14.465H2.13643C1.5088 14.465 1 13.9708 1 13.3612Z" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 12.2574H4.40928" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.68213 12.2574H7.25034" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1.56824 3.97867L3.27288 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M15.2054 3.97867L13.5007 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M4.46582 2.87482C6.62503 0.777565 10.148 0.777565 12.3072 2.87482" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.51733 4.56583C7.49311 3.61909 9.16586 3.61909 10.1416 4.56583" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M12.9325 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.33001 7.1486C8.1731 7.1486 8.0459 7.02519 8.0459 6.87294C8.0459 6.72071 8.1731 6.59729 8.33001 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.32996 7.1486C8.48686 7.1486 8.61406 7.02519 8.61406 6.87294C8.61406 6.72071 8.48686 6.59729 8.32996 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1 13.3612V11.1535C1 10.5439 1.5088 10.0497 2.13643 10.0497H14.6371C15.2648 10.0497 15.7736 10.5439 15.7736 11.1535V13.3612C15.7736 13.9708 15.2648 14.465 14.6371 14.465H2.13643C1.5088 14.465 1 13.9708 1 13.3612Z" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 12.2574H4.40928" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.68213 12.2574H7.25034" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1.56824 3.97867L3.27288 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M15.2054 3.97867L13.5007 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M4.46582 2.87482C6.62503 0.777565 10.148 0.777565 12.3072 2.87482" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.51733 4.56583C7.49311 3.61909 9.16586 3.61909 10.1416 4.56583" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M12.9325 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.33001 7.1486C8.1731 7.1486 8.0459 7.02519 8.0459 6.87294C8.0459 6.72071 8.1731 6.59729 8.33001 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.32996 7.1486C8.48686 7.1486 8.61406 7.02519 8.61406 6.87294C8.61406 6.72071 8.48686 6.59729 8.32996 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1 13.3612V11.1535C1 10.5439 1.5088 10.0497 2.13643 10.0497H14.6371C15.2648 10.0497 15.7736 10.5439 15.7736 11.1535V13.3612C15.7736 13.9708 15.2648 14.465 14.6371 14.465H2.13643C1.5088 14.465 1 13.9708 1 13.3612Z" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 12.2574H4.40928" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.68213 12.2574H7.25034" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1.56824 3.97867L3.27288 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M15.2054 3.97867L13.5007 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M4.46582 2.87482C6.62503 0.777565 10.148 0.777565 12.3072 2.87482" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.51733 4.56583C7.49311 3.61909 9.16586 3.61909 10.1416 4.56583" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M12.9325 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.33001 7.1486C8.1731 7.1486 8.0459 7.02519 8.0459 6.87294C8.0459 6.72071 8.1731 6.59729 8.33001 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.32996 7.1486C8.48686 7.1486 8.61406 7.02519 8.61406 6.87294C8.61406 6.72071 8.48686 6.59729 8.32996 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1 13.3612V11.1535C1 10.5439 1.5088 10.0497 2.13643 10.0497H14.6371C15.2648 10.0497 15.7736 10.5439 15.7736 11.1535V13.3612C15.7736 13.9708 15.2648 14.465 14.6371 14.465H2.13643C1.5088 14.465 1 13.9708 1 13.3612Z" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 12.2574H4.40928" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.68213 12.2574H7.25034" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1.56824 3.97867L3.27288 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M15.2054 3.97867L13.5007 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M4.46582 2.87482C6.62503 0.777565 10.148 0.777565 12.3072 2.87482" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.51733 4.56583C7.49311 3.61909 9.16586 3.61909 10.1416 4.56583" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M12.9325 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.33001 7.1486C8.1731 7.1486 8.0459 7.02519 8.0459 6.87294C8.0459 6.72071 8.1731 6.59729 8.33001 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.32996 7.1486C8.48686 7.1486 8.61406 7.02519 8.61406 6.87294C8.61406 6.72071 8.48686 6.59729 8.32996 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1 13.3612V11.1535C1 10.5439 1.5088 10.0497 2.13643 10.0497H14.6371C15.2648 10.0497 15.7736 10.5439 15.7736 11.1535V13.3612C15.7736 13.9708 15.2648 14.465 14.6371 14.465H2.13643C1.5088 14.465 1 13.9708 1 13.3612Z" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 12.2574H4.40928" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.68213 12.2574H7.25034" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1.56824 3.97867L3.27288 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M15.2054 3.97867L13.5007 10.0497" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M4.46582 2.87482C6.62503 0.777565 10.148 0.777565 12.3072 2.87482" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.51733 4.56583C7.49311 3.61909 9.16586 3.61909 10.1416 4.56583" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M12.9325 14.465V15.5688" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.33001 7.1486C8.1731 7.1486 8.0459 7.02519 8.0459 6.87294C8.0459 6.72071 8.1731 6.59729 8.33001 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.32996 7.1486C8.48686 7.1486 8.61406 7.02519 8.61406 6.87294C8.61406 6.72071 8.48686 6.59729 8.32996 6.59729" stroke="{color}" stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1 13.3612V11.1535C1 10.5439 1.5088 10.0497 2.13643 10.0497H14.6371C15.2648 10.0497 15.7736 10.5439 15.7736 11.1535V13.3612C15.7736 13.9708 15.2648 14.465 14.6371 14.465H2.13643C1.5088 14.465 1 13.9708 1 13.3612Z" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 14.465V15.5688" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M3.84106 12.2574H4.40928" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.68213 12.2574H7.25034" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M1.56824 3.97867L3.27288 10.0497" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M15.2054 3.97867L13.5007 10.0497" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M4.46582 2.87482C6.62503 0.777565 10.148 0.777565 12.3072 2.87482" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M6.51733 4.56583C7.49311 3.61909 9.16586 3.61909 10.1416 4.56583" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M12.9325 14.465V15.5688" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.33001 7.1486C8.1731 7.1486 8.0459 7.02519 8.0459 6.87294C8.0459 6.72071 8.1731 6.59729 8.33001 6.59729" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   <path d="M8.32996 7.1486C8.48686 7.1486 8.61406 7.02519 8.61406 6.87294C8.61406 6.72071 8.48686 6.59729 8.32996 6.59729" stroke={color} stroke-linecap="round" stroke-linejoin="round"/>
-   </svg>
-</a>
-   <a class={url == '/SubnetCalculator' ? 'aSelected' : 'aDefault'} href="/SubnetCalculator">SubnetCalculator</a>
+	<a href="/SubnetCalculator">
+		<svg
+			fill="{color}"
+			height="40px"
+			width="35px"
+			version="1.1"
+			id="Layer_1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			viewBox="-46 -46 552.00 552.00"
+			xml:space="preserve"
+			stroke="{color}"
+			><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+				id="SVGRepo_tracerCarrier"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke="#CCCCCC"
+				stroke-width="1.8400000000000003"
+			></g><g id="SVGRepo_iconCarrier">
+				<g id="XMLID_241_">
+					<g>
+						<path
+							d="M369.635,0H90.365C73.595,0,60,13.595,60,30.365v399.27C60,446.405,73.595,460,90.365,460h279.27 c16.77,0,30.365-13.595,30.365-30.365V30.365C400,13.595,386.405,0,369.635,0z M108.204,343.61v-43.196 c0-3.451,2.797-6.248,6.248-6.248h43.196c3.451,0,6.248,2.797,6.248,6.248v43.196c0,3.451-2.797,6.248-6.248,6.248h-43.196 C111.001,349.858,108.204,347.06,108.204,343.61z M108.204,256.61v-43.196c0-3.451,2.797-6.248,6.248-6.248h43.196 c3.451,0,6.248,2.797,6.248,6.248v43.196c0,3.451-2.797,6.248-6.248,6.248h-43.196C111.001,262.858,108.204,260.06,108.204,256.61 z M308.891,421H151.109c-11.046,0-20-8.954-20-20c0-11.046,8.954-20,20-20h157.782c11.046,0,20,8.954,20,20 C328.891,412.046,319.937,421,308.891,421z M208.402,294.165h43.196c3.451,0,6.248,2.797,6.248,6.248v43.196 c0,3.451-2.797,6.248-6.248,6.248h-43.196c-3.451,0-6.248-2.797-6.248-6.248v-43.196 C202.154,296.963,204.951,294.165,208.402,294.165z M202.154,256.61v-43.196c0-3.451,2.797-6.248,6.248-6.248h43.196 c3.451,0,6.248,2.797,6.248,6.248v43.196c0,3.451-2.797,6.248-6.248,6.248h-43.196C204.951,262.858,202.154,260.06,202.154,256.61 z M345.548,349.858h-43.196c-3.451,0-6.248-2.797-6.248-6.248v-43.196c0-3.451,2.797-6.248,6.248-6.248h43.196 c3.451,0,6.248,2.797,6.248,6.248v43.196h0C351.796,347.061,348.999,349.858,345.548,349.858z M345.548,262.858h-43.196 c-3.451,0-6.248-2.797-6.248-6.248v-43.196c0-3.451,2.797-6.248,6.248-6.248h43.196c3.451,0,6.248,2.797,6.248,6.248v43.196h0 C351.796,260.061,348.999,262.858,345.548,262.858z M354,149.637c0,11.799-9.565,21.363-21.363,21.363H127.364 C115.565,171,106,161.435,106,149.637V62.363C106,50.565,115.565,41,127.364,41h205.273C344.435,41,354,50.565,354,62.363V149.637 z"
+						></path>
+					</g>
+				</g>
+			</g></svg
+		>
+	</a>
+	<a class={url == '/SubnetCalculator' ? 'aSelected' : 'aDefault'} href="/SubnetCalculator"
+		>SubnetCalculator</a
+	>
 </div>
